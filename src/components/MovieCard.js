5b83c59b9be5e171dfd817311e1a82a388ae50b8
chungleee <leon.karho.chung@gmail.com>
2021-04-21 19:28:33 -0400

    test taildwind classes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MovieCard = ({ movieData }) => {
 	return (
 		<div>
-			<h1>{movieData.Title}</h1>
+			<h1 className='text-5xl bg-black'>{movieData.Title}</h1>
 			<img src={movieData.Poster} alt={movieData.Title} />
 			<h2>{movieData.Year}</h2>
 			<h3>{movieData.Type}</h3>
