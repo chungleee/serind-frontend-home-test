87e700065ac4dfc20804b8c91ba32deb6aed01a2
chungleee <leon.karho.chung@gmail.com>
2021-04-21 15:20:22 -0400

    page folder for different page components

---
+++ src/pages/MovieDetails.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MovieDetails = () => {
+	return <div>this is the movie details page</div>;
+};
+
+export default MovieDetails;
---
+++ src/pages/Movies.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Movies = () => {
+	return <div>this is the movies list home page</div>;
+};
+
+export default Movies;
