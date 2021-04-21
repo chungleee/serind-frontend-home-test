08bd61de6813c063ee0084f273b2714390616eb1
chungleee <leon.karho.chung@gmail.com>
2021-04-21 18:49:23 -0400

    set movies handler function to pass data from child component to parent - movie card mapped once movies state is set

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import MovieCard from '../components/MovieCard';
+import Search from '../components/Search';
 
 const Movies = () => {
-	return <div>this is the movies list home page</div>;
+	const [movies, setMovies] = useState();
+
+	console.log('movies', movies);
+
+	const handleSetMovies = (moviesData) => {
+		setMovies(moviesData);
+	};
+
+	return (
+		<div>
+			<Search handleSetMovies={handleSetMovies} />
+
+			{movies &&
+				movies.map((movie) => {
+					return <MovieCard key={movie.imdbID} movieData={movie} />;
+				})}
+		</div>
+	);
 };
 
 export default Movies;
