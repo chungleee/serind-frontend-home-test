69d56e2653fe078fa245f254cb7c6b182ba6e2c7
chungleee <leon.karho.chung@gmail.com>
2021-04-21 15:20:06 -0400

    remove boilerplate - import react router and initial route setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import MovieDetails from './pages/MovieDetails';
+import Movies from './pages/Movies';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		// Router setup
+		<Router>
+			<Route exact path='/' component={Movies} />
+			<Route exact path='/movie-details' component={MovieDetails} />
+		</Router>
+	);
 }
 
 export default App;
