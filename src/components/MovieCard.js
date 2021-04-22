b667f569bb852cabff3a6c2ebb116cfa6a209a9a
chungleee <leon.karho.chung@gmail.com>
2021-04-21 20:23:40 -0400

    styling + link to movie details passing movie id as link param

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MovieCard = ({ movieData }) => {
 	return (
-		<div>
-			<h1 className='text-5xl bg-black'>{movieData.Title}</h1>
+		<div className='w-2/6 mb-8' style={{ minHeight: '500px' }}>
+			<h1 className='text-5xl'>{movieData.Title}</h1>
 			<img src={movieData.Poster} alt={movieData.Title} />
-			<h2>{movieData.Year}</h2>
-			<h3>{movieData.Type}</h3>
+			<h2>Year: {movieData.Year}</h2>
+			<h3>Type: {movieData.Type}</h3>
+			<Link to={`/${movieData.imdbID}`}>
+				<button className='border border-solid border-green-400 w-20'>
+					Details
+				</button>
+			</Link>
 		</div>
 	);
 };
